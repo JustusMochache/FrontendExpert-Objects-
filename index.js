f6900051fe601a4d924f34d955c6d4d947ee9198
Justus <justus.onyancha@moringaschool.com>
2023-01-03 07:04:46 +0300

    add object

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Testing js");
+
+const website = {
+    name: "AlgoSchool",
+    founders: ["Justus", "Diane"],
+    rating: 5,
+    isawesome: false,
+    
+
+}
+
+console.log(website);
